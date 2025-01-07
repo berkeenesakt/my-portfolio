4d35c6df8799f3340dbee33bb11b89d3bfe5a0cd
Berke Enes Aktümen <81863556+berkeenesakt@users.noreply.github.com>
2025-01-07 13:02:46 +0300

    remove unused files and dependencies from iOS and Android projects

--- lib/product/navigation/app_router.dart
+++ lib/product/navigation/app_router.dart
@@ -1,20 +1,17 @@
-import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:template/feature/home/view/home_view.dart';
-import 'package:template/feature/login/view/login_view.dart';
 
-part 'app_router.gr.dart';
+class AppRouter {
+  final router = GoRouter(
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const HomeView(),
+      ),
+      // Add more routes here
+    ],
+  );
 
-@AutoRouterConfig(replaceInRouteName: AppRouter._replaceRouteName)
-class AppRouter extends RootStackRouter {
-  static const _replaceRouteName = 'View|Page,Route';
-  @override
-  List<AutoRoute> get routes => [
-        AutoRoute(page: HomeRoute.page, initial: true),
-        AutoRoute(page: LoginRoute.page),
-      ];
-
-  @override
-  List<AutoRouteGuard> get guards => [
-        // optionally add root guards here
-      ];
+  RouterConfig<Object> config() => router;
 }
